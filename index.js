ad3edbea2306e78b2c5f702e3aac894ae7dcf211
Lorenzo Bugiani <lorenzo.bugiani@viralize.com>
2018-12-03 19:21:51 +0100

    fixed import common

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = {
-  extends: [
-    'eslint:recommended'
-    'airbnb/legacy',
-    'viralize-base'
-  ].map(require.resolve),
-  rules: {
-    "func-names": "off",
-    "strict": ["error", "function"],
-    "wrap-iife": ["error", "inside"]
-  }
+    extends: [
+        'eslint:recommended',
+        'airbnb/legacy',
+        'viralize-common'
+    ].map(require.resolve),
+    rules: {
+        'func-names': 'off',
+        strict: ['error', 'function'],
+        'wrap-iife': ['error', 'inside']
+    }
 };
